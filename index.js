86eb0cd9fddccddee15a59e8090abe99a9d6aac5
irinaog <goncharirina08@gmail.com>
2022-09-13 14:56:50 +0300

    create function

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 console.log('Hello!')
+const contacts = require('./contacts');
+// listContacts();
+contacts.getContactById(8);
+// contacts.listContacts();
+// contacts.removeContact(3);
+// contacts.addContact('Mary', "@gmail.com", 2208);
